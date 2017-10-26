f34464d505fc02c809444be0e6cd8efe9a2af0cf
Chris Johnson and Colin Worf <chris@>
2017-10-26 16:30:37 -0700

    add notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 function encrypt(sentence){
   var nearestSquare = Math.floor(Math.sqrt(sentence.length));
+  var column = new RegExp('.{1,' + nearestSquare + '}', 'g'); //   /.{1,x}/g
+  var groups = sentence.match(column);
+  var encryptedArray = [];
+  for (var i = 0; i < groups.length; i++) {
+    groups.forEach(function(group){
+      encryptedArray.push(group.charAt(i));
+    });
+  console.log(encryptedArray);
+  }
+  return encryptedArray;
+    
+  }
+
+function decrypt(sentence) {
+  var nearestSquare = Math.ceil(Math.sqrt(sentence.length));
   var column = new RegExp('.{1,' + nearestSquare + '}', 'g');
   var groups = sentence.match(column);
   var encryptedArray = [];
@@ -12,10 +27,6 @@ function encrypt(sentence){
   return encryptedArray;
 }
 
-function decrypt(sentence) {
-
-}
-
 $(function(){
   $('#user-input').submit(function(event){
     event.preventDefault();
@@ -23,6 +34,7 @@ $(function(){
     var encrypted = encrypt(userInput).join("");
     encrypted = encrypted.match(/.{1,5}/g).join(" ");
     $('#result').text(encrypted);
-    var decrypted = decrypt(encrypted);
+    var decrypted = decrypt(encrypted.replace(/ /g,""));
+    $('#result').text("<br>"decrypted);
   });
 });
